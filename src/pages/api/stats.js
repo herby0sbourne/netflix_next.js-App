10c3ff9a9db1d529019501ee0bd27b95e5c6b14b
Herby <herbyosbourne31@gmail.com>
2023-07-05 09:03:27 -0500

    create status api for like and dislike

---
+++ src/pages/api/stats.js
@@ -0,0 +1,57 @@
+import jwt from "jsonwebtoken";
+import { findVideoIdByUserId, insertStats, updateStats } from "lib/db/hasura";
+
+export default async function stats(req, res) {
+  if (!(req.method === "POST")) {
+    return res.send({ msg: "not a Post Method" });
+  }
+  const { videoId, favourited, watched = true } = req.body;
+
+  if (!videoId) {
+    return res.status(401).send({ mgs: "videoId missing" });
+  }
+
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(403).send({});
+    }
+
+    const decoded = await verifyAndDecodeJWT(token, process.env.NEXT_PUBLIC_JWT_SECRET);
+
+    const doesStatusExist = await findVideoIdByUserId(token, decoded.issuer, videoId);
+
+    const data = {
+      userId: decoded.issuer,
+      watched,
+      videoId,
+      favourited
+    };
+
+    if (doesStatusExist) {
+      // update it
+      const resp = await updateStats(token, { ...data });
+      return res.send({ data: resp.data });
+    } else {
+      const resp = await insertStats(token, { ...data });
+      return res.send({ data: resp.data });
+    }
+
+    return res.send({ mgs: "it works", findVideoId });
+  } catch (error) {
+    return res.status(500).send({ done: false, error: error.message });
+  }
+}
+
+const verifyAndDecodeJWT = (token, secret) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (error, decoded) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
+};
