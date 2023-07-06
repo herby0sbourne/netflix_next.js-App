8962e34da04fc6e708dfa8b58fe543a455cda3d7
Herby <herbyosbourne31@gmail.com>
2023-07-06 11:40:24 -0500

    add option to wrap card and disable hover

--- components/CardSlider/CardSlider.jsx
+++ components/CardSlider/CardSlider.jsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import Card from "components/Card/Card";
-
+import cls from "classnames";
 import styles from "./CardSlider.module.css";
 
 const CardSlider = (props) => {
-  const { title, videos = [], size } = props;
+  const { title, videos = [], size, isWrap = false, isHover } = props;
   return (
     <section className={styles.section}>
       <div className={styles.container}>
         <h2 className={styles.title}>{title}</h2>
-        <div className={styles.cardWrapper}>
+        <div className={cls(styles.cardWrapper, isWrap && styles.wrap)}>
           {videos.map((video, idx) => {
             return (
               <Link href={`/video/${video.id}`} key={idx}>
-                <Card idx={idx} imgUrl={video.imgUrl} size={size} />
+                <Card idx={idx} imgUrl={video.imgUrl} size={size} isHover={isHover} />
               </Link>
             );
           })}
