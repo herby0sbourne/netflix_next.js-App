ca8dc411d302e3e6235a5054eb3ccb72b8f9b5e5
Herby <herbyosbourne31@gmail.com>
2023-07-05 22:18:55 -0500

    create function to update like/dislike and check like status

---
+++ utils/fetch.js
@@ -0,0 +1,20 @@
+export const updateLike = async (videoId, favourited) => {
+  return await fetch("/api/stats", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      videoId,
+      favourited
+    })
+  });
+};
+
+export const likeStatus = async (videoId) => {
+  const response = await fetch(`/api/stats?videoId=${videoId}`);
+
+  const data = await response.json();
+
+  return data[0]?.favourited;
+};
