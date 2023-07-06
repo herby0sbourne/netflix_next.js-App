b71df2c6427713b07f430fd2d32be94ba149d262
Herby <herbyosbourne31@gmail.com>
2023-07-06 11:08:26 -0500

    create my list page to show liked videos

---
+++ src/pages/browse/my-list.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+
+import redirectUser from "/utils/redirectUser";
+import { getMyLikeVideos } from "/lib/videos";
+
+import NavBar from "/components/NavBar/NavBar";
+import CardSlider from "/components/CardSlider/CardSlider";
+
+import styles from "@/styles/MyList.module.css";
+
+export default function myList({ likeVideos }) {
+  return (
+    <>
+      <Head>
+        <title>favorite video</title>
+      </Head>
+      <main className={styles.main}>
+        <NavBar />
+        <div className={styles.sectionWrapper}>
+          <CardSlider title={"My List"} videos={likeVideos} size="small" wrap />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  const { token, userId } = await redirectUser(context);
+
+  if (!userId) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/login",
+        permanent: false
+      }
+    };
+  }
+
+  const likeVideos = await getMyLikeVideos(token, userId);
+
+  return { props: { likeVideos } };
+};
